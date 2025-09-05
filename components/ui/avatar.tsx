2f29e75bbe38ea76114d0ea9ed8a410b65ae8821
sudhanshu-881 <92862248+sudhanshu-881@users.noreply.github.com>
2025-09-05 16:26:37 -0700

    Update avatar.tsx

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -34,17 +34,14 @@ const AvatarFallback = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Fallback
     ref={ref}
-    className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-muted",
-      className
-    )}
+    className={cn("flex h-full w-full items-center justify-center rounded-full bg-muted", className)}
     {...props}
   />
 ));
 AvatarFallback.displayName = "AvatarFallback";
 
-export function AvatarInitials({ initials }: { initials: string }) {
+const AvatarInitials = ({ initials }: { initials: string }) => {
   return <span>{initials}</span>;
-}
+};
 
 export { Avatar, AvatarImage, AvatarFallback, AvatarInitials };
